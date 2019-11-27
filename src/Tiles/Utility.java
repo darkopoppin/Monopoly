f04be593788bf9b75ed1baab46a733d9f9821243
Bien Jorvina <jorvinb2@winlabs.com>
2019-11-27 19:54:58 +0000

    utility added

---
+++ src/Tiles/Utility.java
@@ -0,0 +1,22 @@
+package Tiles;
+
+import game.Player;
+import game.Board;
+
+public class Utility extends LandTile{
+
+		public Utility(int ID, String name, int value, int rent) {
+			super(ID, name, value, rent);
+		}
+		
+//		//public int calculateRent() {
+//			if(super.getOwner() != getOwner()) {
+//				int [] lst = {4, 12, 28, 38};
+//				int count = 0;
+//				for(int index : lst) {
+//					//if(Board.getOwner(index))
+//				}
+//				return count * rent;
+//			}
+//		}
+}
