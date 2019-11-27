61340987cb7b3f2e7b40975449aca7fd84decd21
Maruko <mjorvina2016@gmail.com>
2019-11-27 19:42:30 +0000

    Property

--- src/Tiles/Property.java
+++ src/Tiles/Property.java
@@ -5,21 +5,50 @@ public class Property extends LandTile{
     private int houses;
     private String colourGroup;
 
-    public Property(int ID, String name, int value, int rent, Boolean mortgageStatus, Player owner, int houses, String colourGroup){
-        super(ID, name, value, rent, mortgageStatus, owner);
+    public Property(int ID, String name, int value, int rent, int houses, String colourGroup){
+        super(ID, name, value, rent);
         this.houses = houses;
         this.colourGroup = colourGroup;
 
     }
 
-    public static void Upgrade(){
-
+    public void Upgrade(){
+        // Increase house count
+        houses ++;
+        
     }
 
     public int calculateRent(){
-        int rent = 0;
-        // Multiplier given the houses * rent
+        // Rent modifier given the houses
+        if(houses != 0){
+        int rent = super.getRent();
+        } else{
+            if(colourGroup == "Purple"){
 
+            }
+            else if(colourGroup == "Teal"){
+
+            }
+            else if(colourGroup == "Pink"){
+
+            }
+            else if(colourGroup == "Orange"){
+
+            }
+            else if(colourGroup == "Red"){
+
+            }
+            else if(colourGroup == "Yellow"){
+
+            }
+            else if(colourGroup == "Green"){
+
+            }
+            else if(colourGroup == "Blue"){
+                
+            }
+
+        }
         return rent;
     }
 
@@ -28,4 +57,5 @@ public class Property extends LandTile{
 
         return colourGroup;
     }
+
 }
