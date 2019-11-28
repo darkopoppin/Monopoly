57652b0df2252e7c0dd1fd04b4cf663b58ae5ac8
Melvin Cotoner <melvincotoner@Melvins-MacBook-Pro.local>
2019-11-28 13:11:17 +0000

    station

---
+++ src/tiles/Station.java
@@ -0,0 +1,18 @@
+package tiles;
+
+public class Station extends LandTile{
+
+    public Station(int ID, String name, int value, int rent){
+        super(ID, name, value, rent);
+    }
+
+    public int calculateRent(){
+        int count = 0;
+        for(int property : propertyOwned){
+            if(true){
+                count++;
+            }
+        return count*rent;
+        }
+    }
+}
