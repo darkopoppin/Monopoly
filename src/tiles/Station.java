14cbe4235bf89a72e7f8166771b02c618ecbd050
Melvin Cotoner <melvincotoner@Melvins-MacBook-Pro.local>
2019-11-28 14:57:41 +0000

    miko

--- src/tiles/Station.java
+++ src/tiles/Station.java
@@ -1,18 +1,31 @@
 package tiles;
 
-public class Station extends LandTile{
+import game.Player;
+import game.Board;
+
+public class Station extends LandTile {
+
+	private int rent;
 
     public Station(int ID, String name, int value, int rent){
         super(ID, name, value, rent);
     }
 
     public int calculateRent(){
+        int [] lst = {5, 15, 25, 35};
         int count = 0;
-        for(int property : propertyOwned){
-            if(true){
+        for(int index : propertyOwned){
+            if(contains(lst, index)){
                 count++;
             }
-        return count*rent;
         }
+        return count*rent;
     }
+
+    private boolean contains(int[] lst, int index) {
+		for(int i : lst){
+			if(i == index) return true;
+		}
+		return false;
+	}
 }
